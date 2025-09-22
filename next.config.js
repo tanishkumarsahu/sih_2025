116795eb32ac9c0eebe4e9db234b49b64d53bd59
tanishkumarsahu <tanishsahu9331@gmail.com>
2025-09-22 19:44:19 +0530

    vercel ignored

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
